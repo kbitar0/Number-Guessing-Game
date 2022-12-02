02114f6908ee973074d024821ea21bcf760a2f15
kbitar0 <110556639+kbitar0@users.noreply.github.com>
2022-12-01 19:25:00 -0500

    Update Number Guessing Game.py

--- Number Guessing Game/Number Guessing Game.py
+++ Number Guessing Game/Number Guessing Game.py
@@ -1,5 +1,3 @@
-#This code will first select a random integer and assign it to the variable "prize". It will then take the bids of 4 different players and use if statements to compare them to the actual number. 
-#The winner will be determined by whoevers guess was closest to the actual number. If someone guesses the exact number, they also win an additional 500 dollars. If their guess goes over the actual value, they lose.
 
 #Establish price of prize using randint
 from random import randint
